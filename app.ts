1a457a08663c553ef8d563ac339b57ddde270767
Suraj <surajchan68@gmail.com>
2023-12-14 23:18:42 +0545

    feat: reserve string

--- app.ts
+++ app.ts
@@ -3,3 +3,9 @@ console.log("Hello world");
 const hello = "world";
 
 console.log(hello);
+
+let original = "Hello World";
+let reversed = [...original].reverse().join("");
+console.log(reversed);
+
+console.log(new Date());
