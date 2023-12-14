ae96849a490dfa67a079a2227d37a924c4230cf7
Suraj <surajchan68@gmail.com>
2023-12-14 23:31:33 +0545

    chore: un ignore build

---
+++ build/app.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello world");
+const hello = "world";
+console.log(hello);
+let original = "Hello World";
+let reversed = [...original].reverse().join("");
+console.log(reversed);
